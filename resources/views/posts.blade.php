13c745ac64efed80423d409c3821713ddda642c5
AlessandroVe <aveneziano536@gmail.com>
2021-09-15 03:53:55 +0200

    .

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
-<!-- @foreach($allPosts as $Post)
-            <div>
-                <div class="cover">
-                    <img src="{{$Post['cover']}}" alt="">
-                </div>
-                <div class='description'>
-                    <p>{{$Post['description']}}</p>
-                </div>
-                <div class='likes'>
-                    <p>{{$Post['likes']}}</p>
-                </div>
-            </div>
-@endforeach -->
 <table class="table">
   <thead>
         <tr>
