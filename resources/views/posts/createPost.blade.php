04a984d2945f828f2c6d4af1fec5693f4d0b9cfe
AlessandroVe <aveneziano536@gmail.com>
2021-09-16 16:10:54 +0200

    .

--- resources/views/posts/createPost.blade.php
+++ resources/views/posts/createPost.blade.php
@@ -14,21 +14,28 @@
 @endif
 
 <div class='container'>
-    <form action="{{route('posts.store')}}"method="post">
-
+    <form>
         @csrf
 
-        <label for="description">Description</label>
-        <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        <div class="form-group d-flex align-items-center">
+            <label for="description">Description</label>
+            
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
 
-        <label for="immagine">Immagine</label>
-        <textarea name="cover" id="immagine" cols="30" rows="10"></textarea>
+        <div class="form-group d-flex align-items-center">
+            <label for="immagine">Immagine</label>
+            <textarea name="cover" id="immagine" cols="30" rows="10"></textarea>
+        </div>
 
-        <label for="likes">Likes</label>
-        <input type="number" id="likes" name="likes" min="1" max="5000">
+        <div class="form-check d-flex align-items-center">
+            <label for="likes">Likes</label>
+            <input type="number" id="likes" name="likes" min="1" max="5000">
+        </div>
 
-        <input type="submit" value="Carica">
+        <br>
 
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
 @endsection
